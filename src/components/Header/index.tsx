b0ac9c7de4f90f460bd8e93a53bf0549937c1512
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-25 22:41:09 -0300

    creating dropdown

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,42 @@
-import LogoAlmeidaFest from '../../assets/LogoAlmeidaFest.png'
-import { HeaderContainer } from './styles'
+import { useState } from "react";
+import LogoAlmeidaFest from "../../assets/LogoAlmeidaFest.png";
+import {
+  DropdownButton,
+  DropdownList,
+  DropdownMenu,
+  HeaderContainer,
+} from "./styles";
 
 export function Header() {
-    return (
-        <HeaderContainer>
-        <img src={LogoAlmeidaFest} alt="" />
-        </HeaderContainer>
-    )
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const isSmallScreen = window.innerWidth < 768;
+  return (
+    <HeaderContainer>
+    <div>
+      {isSmallScreen ? (
+        <DropdownButton open={isDropdownOpen} onClick={handleDropdownToggle}>
+          <div className="icon" />
+        </DropdownButton>
+      ) : null}
+      {isDropdownOpen && (
+        <>
+          <DropdownMenu open={isDropdownOpen}>
+            <DropdownList>
+              <a>Tendas</a>
+              <a>Mesas e Cadeiras</a>
+              <a>Freezers</a>
+              <a>Toalhas</a>
+            </DropdownList>
+          </DropdownMenu>
+        </>
+      )}
+      </div>
+      <img src={LogoAlmeidaFest} alt="" />
+    </HeaderContainer>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import { keyframes, styled } from "styled-components";
 
 export const HeaderContainer = styled.div`
    display: flex;
@@ -20,3 +20,81 @@ export const HeaderContainer = styled.div`
      }
   }
 `
+
+const slideIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const DropdownMenu = styled.div<{ open: boolean }>`
+  display: ${(props) => (props.open ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+
+  z-index: 100;
+  position: fixed;
+  top: 110px;
+  right: 0px;
+  width: 100vw;
+  background-color: ${(props) => props.theme["gray-100"]};
+  border-radius: 4px;
+  animation: ${slideIn} 0.3s ease-in-out;
+`;
+
+export const DropdownList = styled.div`
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme["black"]};
+    display: block;
+    padding: 17px 18px;
+    text-align: center;
+    text-transform: uppercase;
+  }
+`;
+
+interface DropdownButtonProps {
+  open: boolean;
+  onClick: () => void;
+}
+
+export const DropdownButton = styled.button<DropdownButtonProps>`
+  position: absolute;
+  z-index: 100;
+  border: 0;
+  padding: 15px 15px;
+  margin-top: -1rem;
+  background: none;
+  left: 30px;
+
+  .icon {
+    width: 24px;
+    height: 2px;
+    background: ${(props) => props.theme["black"]};
+    position: relative;
+    display: flex;
+  }
+
+  .icon:before,
+  .icon:after {
+    content: "";
+    position: absolute;
+    width: 24px;
+    height: 2px;
+    background: ${(props) => props.theme["black"]};
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .icon:before {
+    top: -6px;
+  }
+
+  .icon:after {
+    top: 6px;
+  }
+`;
