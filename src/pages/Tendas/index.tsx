183a47da8d443913d475eb7fd76dd0724fd20666
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-26 20:50:55 -0300

    finishing tendas

--- src/pages/Tendas/index.tsx
+++ src/pages/Tendas/index.tsx
@@ -1,14 +1,67 @@
 import { Header } from "../../components/Header";
 import { SectionLinks } from "../../components/SectionLinks";
 import { WhatsAppButton } from "../../components/WhatsAppButton";
+import {
+  AboutTendaContainer,
+  CheckSize,
+  InformationTendaContainer,
+  TendaImgContainer,
+  TendaSizeContainer,
+  TendaType,
+} from "./styles";
+import TendaPiramide from "../../assets/Tendas/Tenda-Piramide.jpg";
+import TendaChapeuDeBruxa from "../../assets/Tendas/Tenda-Chapeu.jpeg";
+
+import { CheckCircle, WhatsappLogo } from "phosphor-react";
 
 export function Tendas() {
-    return (
-        <>
-        <Header />
-        <SectionLinks />
+  return (
+    <>
+      <Header />
+      <SectionLinks />
 
-        <WhatsAppButton />
-        </>
-    )
+      <AboutTendaContainer>
+        <TendaImgContainer>
+          <img src={TendaChapeuDeBruxa} alt="" />
+        </TendaImgContainer>
+        <InformationTendaContainer>
+        <TendaType>
+          Tendas <span>Chapéu de Bruxa</span>
+        </TendaType>
+        <CheckSize>Confira os TAMANHOS</CheckSize>
+        <TendaSizeContainer>
+          <div>
+            <p><CheckCircle size={22} color="#ff3c04" weight="fill" /> 3x3</p>
+            <p><CheckCircle size={22} color="#ff3c04" weight="fill" /> 4x4</p>
+          </div>
+          <div>
+            <p><CheckCircle size={22} color="#ff3c04" weight="fill" /> 6x6</p>
+          </div>
+        </TendaSizeContainer>
+        <button>Fale Conosco <WhatsappLogo size={22} /></button>
+        </InformationTendaContainer>
+      </AboutTendaContainer>
+
+      <AboutTendaContainer>
+        <TendaImgContainer>
+          <img src={TendaPiramide} alt="" />
+        </TendaImgContainer>
+        <InformationTendaContainer>
+        <TendaType>
+          Tendas <span>Pirâmide</span>
+        </TendaType>
+        <CheckSize>Confira os TAMANHOS</CheckSize>
+        <TendaSizeContainer>
+          <div>
+            <p><CheckCircle size={22} color="#ff3c04" weight="fill" /> 4x4</p>
+            <p><CheckCircle size={22} color="#ff3c04" weight="fill" /> 5x5</p>
+          </div>
+        </TendaSizeContainer>
+        <button>Fale Conosco <WhatsappLogo size={22} /></button>
+        </InformationTendaContainer>
+      </AboutTendaContainer>
+     
+      <WhatsAppButton />
+    </>
+  );
 }
---
+++ src/pages/Tendas/styles.ts
@@ -0,0 +1,117 @@
+import { styled } from "styled-components";
+
+export const AboutTendaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 6rem;
+  text-align: center;
+  margin: 2rem 0 6rem 0;
+
+  @media (max-width: 768px) {
+     flex-direction: column;
+     gap: 0rem;
+    }
+`;
+
+export const InformationTendaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 1rem;
+
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.3rem;
+
+    font-size: 1.1rem;
+    padding: 1rem;
+    width: 20rem;
+    border-radius: 9px;
+    border: 0;
+    background: ${(props => props.theme["green-300"])};
+    cursor: pointer;
+    font-weight: bold;
+    color: ${(props => props.theme["white"])};
+
+    &:hover {
+        background: ${(props => props.theme["green-400"])};
+        transition: 0.3s background-color;
+    }
+  }
+
+  @media (max-width: 768px) {
+      gap: 0.3rem;
+
+      button {
+        width: 12rem;
+        margin: 0 auto;
+        margin-top: 0.8rem;
+      }
+    }
+`;
+
+export const TendaType = styled.h1`
+  color: #ff3c04;
+  font-family: "Roboto Slab";
+  font-weight: 100;
+  font-size: 2.5rem;
+
+  span {
+    font-weight: bold;
+  }
+
+  @media (max-width: 768px) {
+      font-size: 1.5rem;
+      margin: 0 auto;
+    }
+`;
+
+export const CheckSize = styled.h2`
+  color: ${(props => props.theme["gray-700"])};
+
+  @media (max-width: 768px) {
+      font-size: 1.2rem;
+      margin: 0 auto;
+    }
+`;
+
+export const TendaSizeContainer = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    
+    margin-top: 0.5rem;
+
+    p {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.3rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    margin: 0 auto;
+    }
+`;
+
+export const TendaImgContainer = styled.div`
+  display: flex;
+  img {
+    width: 20rem;
+  }
+
+  @media (max-width: 768px) {
+     img {
+        width: 70%;
+        margin: 0 auto;
+     }
+    }
+`;
